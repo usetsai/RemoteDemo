d0f1bb7b6aa6ba50d3f9df4e6ed84521336d5d17
junliang <junliang.cai@dreamsmart.com>
2025-01-01 17:15:24 +0800

    update

--- remotesurfaceservice/build.gradle.kts
+++ remotesurfaceservice/build.gradle.kts
@@ -39,6 +39,5 @@ dependencies {
 
     implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("com.google.android.material:material:1.12.0")
-
-    implementation(project(":RemoteSurfaceLib"))
+    implementation(project(":remotesurfacelib"))
 }
